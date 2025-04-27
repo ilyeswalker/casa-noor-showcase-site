87603a31184fd01be281b07d25761e0805baaed8
ilyeswalker <113775534+ilyeswalker@users.noreply.github.com>
2025-04-27 18:37:29 +0100

    flex

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -35,14 +35,15 @@ const Hero = () => {
       <div className="text-center space-y-8 z-10 animate-fadeIn">
         <div className="mb-4 transform transition-all duration-500 hover:scale-105">
           <h1 className="mb-2">
-          <span className="inline font-regularBrush text-6xl text-casanoor-blue mb-2">
-              Casa 
-          </span>
-          <span className="inline font-regularBrush text-6xl text-casanoor-red mb-2">
-              Noor
-          </span>
 
-
+          <div className="flex items-center">
+            <span className="font-regularBrush text-6xl text-casanoor-blue mb-2">
+                Casa 
+            </span>
+            <span className="font-regularBrush text-6xl text-casanoor-red mb-2">
+                 Noor
+           </span>
+          </div>
             <span className="block font-['Poppins'] text-3xl font-black tracking-wide text-casanoor-black">
               ELECTRIC
             </span>
